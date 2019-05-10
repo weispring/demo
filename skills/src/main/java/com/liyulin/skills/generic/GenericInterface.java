3b26fb0634da3703b45e40f56718c928f5bc9940
li xian chun <2629469408@qq.com>
2019-05-10 22:45:48 +0800

    桥接方法

---
+++ skills/src/main/java/com/liyulin/skills/generic/GenericInterface.java
@@ -0,0 +1,11 @@
+package com.liyulin.skills.generic;
+
+/**
+ * @Auther: lixianchun
+ * @Date: 2019/5/10 22:37
+ * @Description:
+ */
+public interface GenericInterface<T> {
+
+    void setBoyd(T t);
+}
