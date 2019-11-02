82d6c578149aee6ba8a3c82ad1b23129613ca3e9
li.xianchun <li.xianchun@vpclub.cn>
2019-11-02 10:58:41 +0800

    master update

--- webservice/src/main/java/com/liyulin/webservice/MainApplication.java
+++ webservice/src/main/java/com/liyulin/webservice/MainApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MainApplication {
 
+	/**
+	 *
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
 	}
